d08978fecadc824c8ad0ed38ffef6e3a2ec65b0d
BarryBlock88 <cool_beans2011@yahoo.com>
2021-06-20 18:51:20 +0000

    test for heruku again

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "a1b1K99J51Bc0A44O38Ib5Le3HI68")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://diyCookBook:cook@diyCookBook.zjmmc.mongodb.net/diyCookBook?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://diyCookBook:cook@diycookbook.zjmmc.mongodb.net/diyCookBook?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "diyCookBook")
