a0405c5594572674faf94f3b3c3edcccb77a3541
BarryBlock88 <cool_beans2011@yahoo.com>
2021-07-09 13:21:37 +0000

    slider updated/home page & css & js

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,10 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.tabs').tabs();
     $('.modal').modal();
-    $('.pic').slick({
-      arrows: false,
-      fade: true,
-      dots: false,
-      accessibility: false,
-      autoplay: true,
-      speed: 3000,
-      centerMode: true
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
     });
+    setInterval(function() {
+        $('#auto-slide').carousel('next');
+      }, 3000);  
   });
