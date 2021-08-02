fef46400caf0360819b7a0413ca184b2d4ec8315
BarryBlock88 <cool_beans2011@yahoo.com>
2021-08-02 18:49:03 +0000

    gitignore update

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "a1b1K99J51Bc0A44O38Ib5Le3HI68")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://brewBookBoy:brew@clustercodein.zjmmc.mongodb.net/brew_book_block?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "brew_book_block")
